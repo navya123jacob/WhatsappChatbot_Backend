dc169526c8abdecab85b9410fd242b9b8834a611
navya123jacob <navyatjacob@gmail.com>
2024-09-06 20:17:06 +0530

    fallback route

--- src/app.js
+++ src/app.js
@@ -29,7 +29,10 @@ app.use(cookieParser());
 app.use('/api/chatbot', chatbotRoutes);
 app.use('/api/auth', authRoutes);
 
-
+// Fallback route
+app.all("*", (req,res) => {
+    res.status(404).json({ 'error': 'Not Found' });
+});
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
